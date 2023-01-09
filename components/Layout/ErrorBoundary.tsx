500eff74ec0ebf08a9a9195091edc0c7a5c5c2ac
Nate Wiltzius <nwiltzius@wisc.edu>
2023-01-09 16:48:37 -0700

    Error boundary added. Not styled

---
+++ components/Layout/ErrorBoundary.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode
+}
+
+type State = {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI
+
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    // You can use your own error logging service here
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    if (hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    // Return children components in case of no error
+
+    // eslint-disable-next-line react/destructuring-assignment
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
